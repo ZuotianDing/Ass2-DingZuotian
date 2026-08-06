c4326a5eb4231c8533682bef057ea2038c5b3b96
ZuotianDing <zuotianding66@gmail.com>
2026-08-06 09:26:28 +0800

    Create a1_classes.py for Album and AlbumCollection
    Added initial structure for Album and AlbumCollection classes.

---
+++ a1_classes.py
@@ -0,0 +1,5 @@
+"""..."""
+# TODO: Copy your first assignment to this file, commit, then refactor it to use Album and AlbumCollection classes
+
+from album import Album
+from albumcollection import AlbumCollection
